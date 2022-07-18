843b14f7f0e24e824f5aa231427f28fa9dc45e65
NEEL <neelshah682003@gmail.com>
2022-07-18 17:36:00 +0530

    first commit

---
+++ Minimum_Number.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Minimum_Number {
+    public static void main(String[] args) {
+        
+        int[] arr = {1, 23, 9, 18, 0, 56, -1};
+
+        System.out.println(Arrays.toString(arr)+"\n");
+        System.out.println("Minimum = "+min(arr));
+    }   
+    static int min(int[] a)
+    {
+        int min = a[0];
+        for(int i=1;i<a.length;i++)
+        {
+            if(min > a[i])
+            {
+                min = a[i];
+            }
+        }
+        return min;
+    }
+}
