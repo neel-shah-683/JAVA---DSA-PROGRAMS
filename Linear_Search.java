0cc3b7a31c6e712bb8c420a09a89b54040dbb356
NEEL <neelshah682003@gmail.com>
2022-07-18 16:58:15 +0530

    second commit

--- Linear_Search.java
+++ Linear_Search.java
@@ -1,14 +1,23 @@
 public class Linear_Search {
     public static void main(String[] args) {
 
-        int[] arr = { 23, 45, 16, 10, 50, 56, 4 };
+        int[] arr = { 56, 10, 89, 4, 7, 45};
 
-        linear_search(arr);
+        if(arr.length==0)
+        {
+            System.out.println("No Elements in an Array");
+        }
+        else
+        {
+            linear_search(arr);
+        }
     }
 
     static void linear_search(int[] a) {
-        int val = 10, flag = 1, index = 0;
+        int val = 89, flag = 1, index = 0;
+
         for (int i = 0; i < a.length; i++) {
+           
             if (a[i] == val) {
                 flag = 1;
                 index = i;
