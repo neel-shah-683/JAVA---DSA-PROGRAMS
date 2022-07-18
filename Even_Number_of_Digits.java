5712b4f2f7decbd19c746cb38fa18c4980874e51
NEEL <neelshah682003@gmail.com>
2022-07-18 20:27:46 +0530

    second commit

--- Even_Number_of_Digits.java
+++ Even_Number_of_Digits.java
@@ -1,20 +1,19 @@
 public class Even_Number_of_Digits {
  public static void main(String[] args) {
     
-    int [] arr = {121, 345, 2, 6, 7896};
+    int [] arr = {12, 345, 2, 6, 7896, 4568};
 
     computeEvenDigits(arr);
  }   
  static void computeEvenDigits(int[] a)
  {
-        int digit=0,c=0;
+        int c=0;
 
         for(int i=0;i<a.length;i++)
         {
             int count=0;
             while(a[i] > 0)
-            {
-                digit = a[i] % 10; 
+            { 
                 a[i] = a[i] / 10;
                 count++;
             }     
