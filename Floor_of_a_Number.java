e0db859cb46f042bab0b93e1cada340375540144
NEEL <neelshah682003@gmail.com>
2022-07-20 16:41:23 +0530

    first commit

---
+++ Floor_of_a_Number.java
@@ -0,0 +1,31 @@
+public class Floor_of_a_Number {
+    public static void main(String[] args) {
+        
+        int[] arr={2, 3, 5, 9, 14, 16, 18};
+                // 0  1  2  3   4   5   6
+        int ans = search(arr);
+        System.out.println(ans);
+    }
+    static int search(int[] a)
+    {
+        int start=0, end = a.length-1;
+        int val = 4;
+        while(start<=end)
+        {
+            int mid = (start+end)/2;
+            if(val == a[mid])
+            {
+                return a[mid];
+            }
+            else if(val < a[mid])
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }        
+        return a[end];
+    }
+}
