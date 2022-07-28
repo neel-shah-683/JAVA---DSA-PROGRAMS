1a90da0b992b0cf369765a7190fb953cca36b2ae
NEEL <neelshah682003@gmail.com>
2022-07-28 15:25:44 +0530

    first commit

--- Bubble_Sort.java
+++ Bubble_Sort.java
@@ -3,12 +3,11 @@ import java.util.Arrays;
 public class Bubble_Sort {
     public static void main(String[] args) {
         
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {-3, 1, -5, 4, -2};
         bubble(arr);
     }
     static void bubble(int[] a)
     {
-        boolean flag=false;
         for(int i=0;i<a.length;i++)
         {
             for(int j=0;j<a.length-1-i;j++)
@@ -18,12 +17,7 @@ public class Bubble_Sort {
                     int temp = a[j];
                     a[j] = a[j+1];
                     a[j+1] = temp;
-                    flag=true;
                 }
-            }
-            if(!flag)
-            {
-                break;
             }
         }
         System.out.println(Arrays.toString(a));
