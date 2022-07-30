d7bff67dd034a20c67dee87b69f5993b09c92d98
NEEL <neelshah682003@gmail.com>
2022-07-30 17:56:33 +0530

    first commit

---
+++ Fibonacci_Sequence.java
@@ -0,0 +1,15 @@
+public class Fibonacci_Sequence {
+    public static void main(String[] args) {
+        
+        int ans = fibo_seq(3);
+        System.out.println(ans);
+    }
+    static int fibo_seq(int n)
+    {
+        if(n<2)
+        {
+            return n;
+        }
+        return fibo_seq(n-1) + fibo_seq(n-2);
+    }
+}
