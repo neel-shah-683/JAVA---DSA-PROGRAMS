05bf60ceaa335e58e8d96a0e6f00163c0407866a
NEEL <neelshah682003@gmail.com>
2022-07-30 15:51:46 +0530

    second commit

--- String_Palindrome_Check.java
+++ String_Palindrome_Check.java
@@ -13,7 +13,7 @@ public class String_Palindrome_Check {
         }
         else
         {
-            for(int i=0,j=s.length()-1;j>i;i++,j--)
+            for(int i=0,j=s.length()-1;j>=i;i++,j--)
             {
                 if(s.charAt(i)==s.charAt(j))
                 {
