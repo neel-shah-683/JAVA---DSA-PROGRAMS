3c07f81753650f54e99f4b61875ace5d2d525038
NEEL <neelshah682003@gmail.com>
2022-08-05 16:53:41 +0530

    first commit

---
+++ Bitwise_Count_Digits.java
@@ -0,0 +1,12 @@
+public class Bitwise_Count_Digits {
+    public static void main(String[] args) {
+        
+        int n=16,count=0;
+        while(n>0)
+        {
+            n = n>>1;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
