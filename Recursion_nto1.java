471d537319047d00caaf58d10c7c545f7bff1041
NEEL <neelshah682003@gmail.com>
2022-08-17 15:23:09 +0530

    first commit

---
+++ Recursion_nto1.java
@@ -0,0 +1,19 @@
+public class Recursion_nto1 {
+    public static void main(String[] args) {
+        
+        int num = 302100004;
+        int ans = countZERO(num);    
+        System.out.println(ans);
+    }
+    static int count=0;
+    static int countZERO(int n)
+    {
+        if(n==0)
+            return count;
+        if(n%10==0)
+        {
+            count++;
+        }
+        return countZERO(n/10);
+    }
+}
