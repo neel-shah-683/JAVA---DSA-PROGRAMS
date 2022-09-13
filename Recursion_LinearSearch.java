681d4ed77b691a6bd13f9a70347dc4bfd5b43e12
NEEL <neelshah682003@gmail.com>
2022-09-13 17:08:19 +0530

    first commit

--- Recursion_LinearSearch.java
+++ Recursion_LinearSearch.java
@@ -1,26 +1,20 @@
+import java.util.ArrayList;
+
 public class Recursion_LinearSearch{
 
     public static void main(String[] args) {
         
-        int[] arr = {3, 2, 1, 18, 9};
-        int val = 1,i = 0;
-        if(recurLinearSearch(arr, val, i)==true)
-        {
-            System.out.println("Element Present in the Array:");
-        }
-        else
-        {
-            System.out.println("Element Not Found:");
-        }
-    }
-    static boolean recurLinearSearch(int[] a, int val, int i)
+        ArrayList<Integer> al = new ArrayList<>();
+        int[] arr = {3, 9, 1, 18, 9};
+        int val = 9, i = 0;
+       System.out.println(recurLinearSearch(arr, val, i, al)); 
+      }
+    static ArrayList<Integer> recurLinearSearch(int[] a, int val, int i, ArrayList<Integer> al )
     {
-       if(i==a.length)
-            return false;
-        
+        if(i==a.length)
+            return al;
         if(a[i] == val)
-            return true;
-        
-        return recurLinearSearch(a, val, ++i);
+            al.add(i);
+        return recurLinearSearch(a, val, ++i, al);
     }
 }
