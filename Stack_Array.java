5535f5427458f7cc2a4b2e825c1dc5e7b0d0c447
NEEL <neelshah682003@gmail.com>
2022-10-03 22:16:58 +0530

    second commit

--- Stack_Array.java
+++ Stack_Array.java
@@ -1,67 +1,75 @@
-public class Stack_Array {
 
-    int Top = -1;
-    public void push(int data, int[] a)
-    {
-        if(Top >= a.length-1)
-        {
-            System.out.println("Stack is Overflow:");
-            return ;
-        }
-        else
-        {
-            Top++;
-            a[Top] = data;
-        }
-    }
+public class Stack_Array{
 
-    public void printStack(int[] a)
-    {
-        if(Top == -1)
+    static class Stack_OP{
+
+        int[] arr;
+        int size;        
+        int top = -1;
+        Stack_OP(int n)
         {
-            System.out.println("Stack is Underflow:");
-            return ;
+            arr = new int[n];
+            this.size = n;
         }
-        else
+
+        public boolean isEmpty()
         {
-            while(Top>=0)
+            return top == -1;
+        }
+        public void push(int data)
+        {
+            if(top == size - 1)
             {
-                System.out.println(a[Top]);
-                Top--;
+                System.out.println("Stack is Overflow:");
+                return ;
+            }
+            else
+            {
+                top++;
+                arr[top] = data;
+            }
+        }
+        public int pop()
+        {
+            if(isEmpty())
+            {
+                System.out.println("Stack is Underflow:");
+                return -1;
+            }
+            else
+            {
+                int x = arr[top];
+                top--;
+                return x;
+            }
+        }
+        public int peek()
+        {
+            if(isEmpty())
+            {
+                System.out.println("Stack is Underflow:");
+                return -1;
+            }
+            else
+            {
+                return arr[top];
             }
         }
     }
-    public void pop(int[] a)
+    public static void main(String[] args)
     {
-        if(Top == - 1)
+        Stack_OP s1 = new Stack_OP(5);
+
+        s1.push(1);
+        s1.push(2);
+        s1.push(3);
+        s1.push(4);
+        s1.push(5);
+
+        while(!s1.isEmpty())
         {
-            System.out.println("Stack is Underflow:");
-            return ;
-        }
-        else
-        {
-            int x = a[Top];
-            Top--;
-            System.out.println("\n"+x + " is removed from Stack:");
-        }
-    }
-    public static void main(String[] args) {
-        
-        Stack_Array sa = new Stack_Array();
-        int[] arr = new int[5];
-
-        sa.push(1,arr);
-        sa.push(2,arr);
-        sa.push(3,arr);
-        sa.push(4,arr);
-        sa.push(5,arr);
-
-        sa.pop(arr);   
-        sa.pop(arr); 
-
-        System.out.println("\nElements Present in the Stack:");
-        sa.printStack(arr);
-
-               
+            System.out.println(s1.peek());
+            s1.pop();
+        }       
     }
 }
