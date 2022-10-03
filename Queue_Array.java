1303e932a3d3516e4a524a05e2e04da1681fee30
NEEL <neelshah682003@gmail.com>
2022-10-03 22:04:39 +0530

    second commit

---
+++ Queue_Array.java
@@ -0,0 +1,79 @@
+public class Queue_Array {
+    
+    static class Queue{
+        static int[] arr;
+        int size;
+        static int rear = -1;
+
+        Queue(int n)
+        {
+            arr = new int[n];
+            this.size = n; 
+        }
+        public boolean isEmpty()
+        {
+            return rear == - 1;
+        }
+        //enqueue
+        public void push(int data)
+        {
+            if(rear == size - 1)
+            {
+                System.out.println("Queue is Overflow:");
+                return ;
+            }
+            else
+            {
+                rear++;
+                arr[rear] = data;
+            }
+        }
+        //dequeue
+        public int pop()
+        {
+            if(rear == -1)
+            {
+                System.out.println("Queue is Underflow:");
+                return -1;
+            }
+            else
+            {
+                int front = arr[0];
+                for(int i=0;i<rear;i++)
+                {
+                    arr[i] = arr[i+1];
+                }  
+                rear--;
+                return front;        
+            }
+        }
+        public int peek()
+        {
+            if(rear==-1)
+            {
+                System.out.println("Queue is Empty:");
+                return -1;
+            }
+            int x = arr[0];
+            return x;
+
+        }
+    }
+    public static void main(String[] args) {
+        
+        Queue q = new Queue(4);
+
+        q.push(1);
+        q.push(2);
+        q.push(3);
+        q.push(4);
+
+        while(!q.isEmpty())
+        {
+            System.out.println(q.peek());
+            q.pop();
+        }
+
+
+    }
+}
